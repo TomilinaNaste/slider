16ea4cc8b74286e1739317afed519a0708dbeebc
Anastacia Tomilina <anastacia.tomilina@yandex.ru>
2023-09-04 20:07:48 +0300

    первый пуш

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hello')
+const carousel = document.querySelector('.carousel');
+
+const dragging = (e) => {
+    carousel.scrollLeft = e.pageX;
+    
+}
+carousel.addEventListener("mousemove", dragging);
