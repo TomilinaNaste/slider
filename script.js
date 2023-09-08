1bb7c8726bef152cf881525ffe95c5639805cdb7
Tomilina Anastacia <anastacia.tomilina@yandex.ru>
2023-09-08 12:56:09 +0300

    движение мышью

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-console.log('hello')
+
 const carousel = document.querySelector('.carousel');
-const dragging = (e) => {
-    carousel.scrollLeft = e.pageX;
-    
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+    //oбновление значения глобальных переменных при событии нажатия мыши
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
 }
+
+const dragging = (e) => {
+    //прокрутка изображения влево по указателю мыши
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
